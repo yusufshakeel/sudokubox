f907d84bd00de66a17b58f1ce6c87a4308037ceb
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2021-11-05 14:53:36 +0530

    update

--- src/builders/board-builder.js
+++ src/builders/board-builder.js
@@ -2,9 +2,18 @@
 
 const { TOTAL_ROWS } = require('../constants');
 
+/**
+ * This will build the sudoku board.
+ * @param {number[]} input This is the one dimensional array.
+ * @constructor
+ */
 function BoardBuilder(input) {
   const isNewRow = index => (index + 1) % TOTAL_ROWS === 0;
 
+  /**
+   * This will return the sudoku board.
+   * @returns {number[][]}
+   */
   this.build = () => {
     const { board } = input.reduce(
       (result, current, index) => {
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,22 +1,54 @@
 'use strict';
 
+/**
+ * This will return the filtered array.
+ * @param {number[]} array The array can be a row or column.
+ * @param {number} indexToSkip The index in the array to skip.
+ * @return {number[]}
+ */
 const filteredArray = (array, indexToSkip) =>
   array.reduce((result, current, index) => {
     return index === indexToSkip ? result : [...result, current];
   }, []);
 
+/**
+ * This will return row of numbers.
+ * @param {number} rowIndex The row index of the board to select.
+ * @param {number[][]} board The sudoku board.
+ * @returns {number[]}
+ */
 function getRow(rowIndex, board) {
   return board[rowIndex];
 }
 
+/**
+ * This will return column of numbers.
+ * @param {number} columnIndex The column index of the board to select.
+ * @param {number[][]} board The sudoku board.
+ * @returns {number[]}
+ */
 function getColumn(columnIndex, board) {
   return board.reduce((result, row) => [...result, row[columnIndex]], []);
 }
 
+/**
+ * This will return true if value is unique in row.
+ * @param {number} value
+ * @param {number} rowIndex
+ * @param {number[]} row
+ * @returns {boolean}
+ */
 function isUniqueValueInRow(value, rowIndex, row) {
   return !filteredArray(row, rowIndex).includes(value);
 }
 
+/**
+ * This will return true if value is unique in column.
+ * @param {number} value
+ * @param {number} columnIndex
+ * @param {number[]} column
+ * @returns {boolean}
+ */
 function isUniqueValueInColumn(value, columnIndex, column) {
   return !filteredArray(column, columnIndex).includes(value);
 }
