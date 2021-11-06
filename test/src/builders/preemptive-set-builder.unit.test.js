e94a8bfccd26d4c028a08e740d6603af42a9ed43
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2021-11-06 16:35:09 +0530

    update

--- test/src/builders/preemptive-set-builder.unit.test.js
+++ test/src/builders/preemptive-set-builder.unit.test.js
@@ -3,7 +3,8 @@
 const PreemptiveSetBuilder = require('../../../src/builders/preemptive-set-builder');
 
 const {
-  partiallySolvedBoardMarkupToFindPreemptiveSet
+  partiallySolvedBoardMarkupToFindPreemptiveSet,
+  partiallySolvedBoardPreemptiveSet
 } = require('../../test-data/sudoku-puzzle-hard');
 
 describe('PreemptiveSetBuilder', () => {
@@ -14,50 +15,7 @@ describe('PreemptiveSetBuilder', () => {
       const preemptiveSet = preemptiveSetBuilder
         .withMarkup(partiallySolvedBoardMarkupToFindPreemptiveSet)
         .build();
-      expect(preemptiveSet).toStrictEqual([
-        {
-          cells: ['0,3', '2,3'],
-          id: '0,3-2,3',
-          size: 2,
-          values: [5, 9]
-        },
-        {
-          cells: ['2,2', '5,2'],
-          id: '2,2-5,2',
-          size: 2,
-          values: [1, 8]
-        },
-        {
-          cells: ['4,0', '4,1'],
-          id: '4,0-4,1',
-          size: 2,
-          values: [2, 6]
-        },
-        {
-          cells: ['5,0', '5,2'],
-          id: '5,0-5,2',
-          size: 2,
-          values: [1, 8]
-        },
-        {
-          cells: ['5,0', '7,0'],
-          id: '5,0-7,0',
-          size: 2,
-          values: [1, 8]
-        },
-        {
-          cells: ['6,3', '8,3'],
-          id: '6,3-8,3',
-          size: 2,
-          values: [1, 7]
-        },
-        {
-          cells: ['7,6', '7,7'],
-          id: '7,6-7,7',
-          size: 2,
-          values: [1, 5]
-        }
-      ]);
+      expect(preemptiveSet).toStrictEqual(partiallySolvedBoardPreemptiveSet);
     });
   });
 
